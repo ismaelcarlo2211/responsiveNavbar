527a882ea4746b59b407bff1e026dc5ed974bf66
ismaelcarlos <ismaelcarlo2211@gmail.com>
2023-11-27 12:41:43 +0200

    i updated alll code

--- main.js
+++ main.js
@@ -1,1 +1,5 @@
-alert("hello");
+const icone=document.querySelector(".icon");
+const nav=document.querySelector('nav');
+icone.addEventListener("click",()=>{
+    nav.classList.toggle('toggle');
+})
